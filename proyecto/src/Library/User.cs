2f6ab4953874ad65b10cd06d8e77b1eb21602abc
Santiago Cardinal <santiago.cardinal@correo.ucu.edu.uy>
2025-10-17 20:53:33 -0300

    Admin and User

---
+++ proyecto/src/Library/User.cs
@@ -0,0 +1,23 @@
+namespace Program;
+
+public class User
+{
+    private string name;
+    private string mail;
+    private string phone;
+    private string id;
+
+    public string Name { get { return name; } set { name = value; } }
+    public string Mail { get { return mail; } set { mail = value; } }
+    public string Phone { get { return phone; } set { phone = value; } }
+    public string Id { get { return id; } set { id = value; } }
+
+
+    public User(string name, string mail, string phone, string id)
+    {
+        Name = name;
+        Mail = mail;
+        Phone = phone;
+        Id = id;
+    }
+}
