fec7bcd860df57f86bc42af003e136350f93d262
CamilaOrueRodriguez <camilaeorue@gmail.com>
2025-10-18 17:09:04 -0300

    Clases Interaction y FileWriter, ademas interfaz IWriter

---
+++ proyecto/src/Library/FileWriter.cs
@@ -0,0 +1,10 @@
+namespace Library;
+
+public class FileWriter: IWriter
+{
+    public string path { get; set; }
+    public void Write(Interaction interaction)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ proyecto/src/Library/Interaction.cs
@@ -0,0 +1,47 @@
+namespace Library;
+
+public class Interaction
+{
+    private DateTime date;
+    private string topic;
+    private ExchangeType type;
+    private List<Note> note;
+
+    public DateTime Date
+    {
+        get { return date; }
+        set { date = value; }
+    }
+
+    public string Topic
+    {
+        get { return topic; }
+        set { topic = value; }
+    }
+
+    public ExchangeType Type
+    {
+        get { return type; }
+        set { type = value; }
+    }
+
+    public List<Note> Note
+    {
+        get { return note; }
+        set { note = value; }
+    }
+
+    public Interaction(DateTime date, string topic, ExchangeType type)
+    {
+        this.Date = date;
+        this.Topic = topic;
+        this.Type = type;
+    }
+
+    public void AddNote(Note note)
+    {
+        this.Note.Add(note);
+    }
+    
+    
+}
