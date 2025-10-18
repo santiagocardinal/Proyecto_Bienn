fd2a737c4635c70ec53c7ca76d444f159554ac44
Santiago Cardinal <santiago.cardinal@correo.ucu.edu.uy>
2025-10-18 17:24:32 -0300

    Some changes

--- proyecto/src/Library/Interaction.cs
+++ proyecto/src/Library/Interaction.cs
@@ -42,6 +42,4 @@ public class Interaction
     {
         this.Note.Add(note);
     }
-    
-    
 }
--- proyecto/src/Library/Quote.cs
+++ proyecto/src/Library/Quote.cs
@@ -1,25 +1,26 @@
+using System.Data.Common;
+
 namespace Library;
 
 public class Quote : Interaction
 {
-    private string date;
     private double amount;
     private string description;
 
-    public string Date { get { return date; } set { date = value; } }
     public double Amount { get { return amount; } set { amount = value; } }
     public string Description { get { return description; } set { description = value; } }
 
-    public Quote(string date, double amount, string description)
+    public Quote(DateTime date, string topic, ExchangeType type, double amount, string description) : base (date, topic, type)
     {
-        this.Date = date;
         this.Amount = amount;
         this.Description = description;
     }
     
-    public void Sale(string date, double amount, string description)
+    public void Sale(DateTime date, string topic, ExchangeType type, double amount, string description)
     {
         this.Date = date;
+        this.Topic = topic;
+        this.Type = type;
         this.Amount = amount;
         this.Description = description;
     }
