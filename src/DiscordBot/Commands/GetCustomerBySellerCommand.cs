c117120316d12fe9107ca4355bbd79e352d63cdd
Lucia Rodriguez Penades <lucia.rodriguezp@correo.ucu.edu.uy>
2025-11-24 09:18:24 -0300

    GetCustomerBySellerCommand

---
+++ src/DiscordBot/Commands/GetCustomerBySellerCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+using Library;
+
+namespace Library
+{
+    /// <summary>
+    /// Comando para obtener todos los clientes asignados a un vendedor.
+    /// Uso: !getCustomerBySellerId V1
+    /// </summary>
+    public class GetCustomerBySellerIdCommand : ModuleBase<SocketCommandContext>
+    {
+        [Command("getCustomerBySellerId")]
+        [Summary("Muestra todos los clientes asociados a un vendedor usando su ID.")]
+        public async Task ExecuteAsync(string sellerId)
+        {
+            string result = Facade.ShowCustomers_BySellerId(sellerId);
+            await ReplyAsync(result);
+        }
+    }
+}
