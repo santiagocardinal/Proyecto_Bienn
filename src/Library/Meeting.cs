edd53bfdcbb355f844a117f420b5ea9c34f985e1
Santiago Cardinal <santiago.cardinal@correo.ucu.edu.uy>
2025-11-30 18:17:49 -0300

    Some changes

--- src/Library/Meeting.cs
+++ src/Library/Meeting.cs
@@ -22,7 +22,7 @@ public class Meeting : Interaction
     public Meeting(string place, DateTime date, string topic, ExchangeType type, Customer _customer) 
         : base(date, topic, type, _customer)
     {
-        // Solo inicializa lo específico de Meeting
+        // Solo inicializamos lo específico de Meeting
         this.Place = place;
     }
 }
